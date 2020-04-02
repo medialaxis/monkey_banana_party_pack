58c106c9053f35945385c2b409b74651ffc20ea0
Andreas Edlund <andreas@myrstacken.net>
2020-04-02 22:43:34 +0200

    Separated color from face

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,14 +1,31 @@
-smiley()
+# https://wiki.archlinux.org/index.php/Bash/Prompt_customization
+
+bold="$(tput bold)"
+red="$(tput setaf 1)"
+green="$(tput setaf 2)"
+yellow="$(tput setaf 3)"
+blue="$(tput setaf 4)"
+reset="$(tput sgr0)"
+
+smiley_color()
 {
     if [ $? = 0 ]; then
-        tput bold
-        tput setaf 2 # Green
-        echo ":)"
+        echo -n "${bold}${green}"
+        return 0
     else
-        tput bold
-        tput setaf 1 # Red
-        echo ":("
+        echo -n "${bold}${red}"
+        return 1
     fi
 }
 
-PS1="\[\033[01;32m\]\u@\h\[\033[01;33m\] \w\n$(smiley) \[\033[01;34m\]\$\[\033[00m\] "
+smiley_face()
+{
+    if [ $? = 0 ]; then
+        echo -n ":)"
+    else
+        echo -n ":("
+    fi
+}
+
+# PS1="\[\033[01;32m\]\u@\h\[\033[01;33m\] \w\n$(smiley) \[\033[01;34m\]\$\[\033[00m\] "
+PS1="\[${bold}${green}\]\u@\h\[${bold}${yellow}\] \w\n\[\$(smiley_color)\]\$(smiley_face) ${bold}${blue}\$${reset} "
