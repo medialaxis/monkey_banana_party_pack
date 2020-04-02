d91f0cc66fa2aecfeaa1978974347e497b6184ca
Andreas Edlund <andreas@myrstacken.net>
2020-04-02 21:11:27 +0200

    Simplified the smiley

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,26 +1,14 @@
-green='\e[1;32m'
-red='\e[1;31m'
-
-smiley_color()
+smiley()
 {
     if [ $? = 0 ]; then
-        echo -e "${green}"
-        return 0
+        tput bold
+        tput setaf 2 # Green
+        echo ":)"
     else
-        echo -e "${red}"
-        return 1
+        tput bold
+        tput setaf 1 # Red
+        echo ":("
     fi
 }
 
-smiley_face()
-{
-    if [ $? = 0 ]; then
-        echo -n ":)"
-        return 0
-    else
-        echo -n ":("
-        return 1
-    fi
-}
-
-PS1="\[\033[01;32m\]\u@\h\[\033[01;33m\] \w\n\[\$(smiley_color)\]\$(smiley_face) \[\033[01;34m\]\$\[\033[00m\] "
+PS1="\[\033[01;32m\]\u@\h\[\033[01;33m\] \w\n$(smiley) \[\033[01;34m\]\$\[\033[00m\] "
