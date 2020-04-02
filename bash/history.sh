c34574fb95f3596a59564ebb892af3e7a69c7285
Andreas Edlund <andreas@myrstacken.net>
2020-04-02 20:45:15 +0200

    Use ignoredups

--- bash/history.sh
+++ bash/history.sh
@@ -11,10 +11,10 @@ shopt -s histappend
 
 # Causes all previous lines matching the current line to be removed from the
 # history list before that line is saved
-#export HISTCONTROL=ignoredups
+export HISTCONTROL=ignoredups
 
 # Erase all matching lines before adding the new one
-export HISTCONTROL=erasedups
+# export HISTCONTROL=erasedups
 
 # Do not truncate history file when this variable is unset
 export HISTFILESIZE=1000000
