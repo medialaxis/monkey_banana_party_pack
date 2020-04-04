37fc80c97a404f4190d650131b1bb65ee8322b23
Andreas Edlund <andreas@myrstacken.net>
2020-04-05 00:11:17 +0200

    Update statusline

--- bin/myi3status.py
+++ bin/myi3status.py
@@ -8,14 +8,21 @@ def system_state():
     cp = subprocess.run('systemctl show --property=SystemState', shell=True, capture_output=True)
     return cp.stdout.decode().strip().split('=')[1]
 
+def update_status(status):
+    state = {
+            "name": "system_state",
+            "color": "#00FF00",
+            "full_text": system_state(),
+            }
+    return [state] + status
+
 def main(argv):
     with subprocess.Popen('i3status', shell=True, stdout=subprocess.PIPE) as ps:
         for line in ps.stdout:
             try:
-                print(json.loads(line[1:]))
+                print(json.dumps(update_status(json.loads(line[1:]))))
             except json.JSONDecodeError:
                 pass
 
 if __name__ == '__main__':
-    print(system_state())
     main(sys.argv)
