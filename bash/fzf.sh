39306150e3e5f8272f0a9960c8d67437221a3db4
Andreas Edlund <andreas@myrstacken.net>
2020-05-11 20:11:49 +0200

    Do not output the same path twice

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -3,5 +3,5 @@ source /usr/share/fzf/completion.bash
 source /usr/share/fzf/key-bindings.bash
 _fzf_setup_completion path git okular vim
 export FZF_DEFAULT_COMMAND='fd --type f'
-command -v blsd > /dev/null && export FZF_ALT_C_COMMAND='blsd'
+export FZF_ALT_C_COMMAND='systool list_dir'
 
