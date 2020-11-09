08918b60db10c1fd2cb4582a4c54725b0e801ebc
Andreas Edlund <andreas@myrstacken.net>
2020-11-09 20:26:50 +0100

    Changed terminal

--- bash/terminal.sh
+++ bash/terminal.sh
@@ -1,7 +1,7 @@
 # Used by i3-sensible-terminal for starting terminals in i3.
 # export TERMINAL=xterm
-# export TERMINAL=xfce4-terminal
-export TERMINAL=urxvt
+export TERMINAL=xfce4-terminal
+# export TERMINAL=urxvt
 
 # Turn on 256 color support...
 if [ "x$TERM" = "xxterm" ]
