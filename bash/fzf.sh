969f2e77f175c2b81e3628589752a461573f40cc
Andreas Edlund <andreas@myrstacken.net>
2021-06-18 18:31:33 +0200

    Added nvim to fzf completion

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -1,6 +1,6 @@
 # Enable FZF completion (**<tab>)
 source /usr/share/fzf/completion.bash
 source /usr/share/fzf/key-bindings.bash
-_fzf_setup_completion path git okular vim mplayer
+_fzf_setup_completion path git okular vim mplayer nvim nv
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_ALT_C_COMMAND='systool list_dir'
