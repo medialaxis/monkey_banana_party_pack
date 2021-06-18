cd223929385d3879577b4da0ec013c1c559f68dc
Andreas Edlund <andreas@myrstacken.net>
2021-06-18 19:53:17 +0200

    Use filelist on ctrl-t

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -4,3 +4,4 @@ source /usr/share/fzf/key-bindings.bash
 _fzf_setup_completion path git okular vim mplayer nvim nv
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_ALT_C_COMMAND='dirlist'
+export FZF_CTRL_T_COMMAND='filelist'
