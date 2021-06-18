9278948ee67a01e04aa7ffc26fe2a715eae2ace9
Andreas Edlund <andreas@myrstacken.net>
2021-06-18 19:30:32 +0200

    Stop using the dir_history thing

--- bash/history.sh
+++ bash/history.sh
@@ -1,6 +1,3 @@
-# Deduplicate the directory history file
-sort -u ~/.local/dir_history/history -o ~/.local/dir_history/history
-
 # Append history to history files after every command, not just when existing
 # bash.
 export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
