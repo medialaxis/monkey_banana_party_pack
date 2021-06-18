3fe32cfb38ce3f555121eaccacf00638c264d63d
Andreas Edlund <andreas@myrstacken.net>
2021-06-18 19:31:23 +0200

    Use dirlist in fzf

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -3,4 +3,4 @@ source /usr/share/fzf/completion.bash
 source /usr/share/fzf/key-bindings.bash
 _fzf_setup_completion path git okular vim mplayer nvim nv
 export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_ALT_C_COMMAND='systool list_dir'
+export FZF_ALT_C_COMMAND='dirlist'
