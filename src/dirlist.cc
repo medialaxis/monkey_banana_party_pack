5d2dbcf245bb27d37e4022546b8630c25dc63844
Andreas Edlund <andreas@myrstacken.net>
2021-06-23 21:59:24 +0200

    Implemented exactly the python dirlist

--- src/dirlist.cc
+++ src/dirlist.cc
@@ -5,11 +5,17 @@
 using std::filesystem::path;
 using std::filesystem::current_path;
 using std::filesystem::directory_iterator;
+using std::filesystem::filesystem_error;
 
 int main(int argc, char *argv[])
 {
+    path home(getenv("HOME"));
+
     std::deque<path> work;
     work.push_back(current_path());
+    work.push_back(home/"wc");
+    work.push_back("/mnt/extra");
+    work.push_back("/run/media");
 
     while (!work.empty()) {
         path curr = work.front();
@@ -17,16 +23,20 @@ int main(int argc, char *argv[])
 
         std::cout << curr.string() << "\n";
 
-        for (auto& dir : directory_iterator(curr)) {
-            if (dir.is_symlink()) continue;
-            if (!dir.is_directory()) continue;
-            if (dir.path().filename() == ".git") continue;
-            if (dir.path().filename() == ".mypy_cache") continue;
-            if (dir.path().filename() == ".cache") continue;
-            if (dir.path().filename() == "__pycache__") continue;
-            if (dir.path().filename() == ".stack-work") continue;
+        try {
+            for (auto& dir : directory_iterator(curr)) {
+                if (dir.is_symlink()) continue;
+                if (!dir.is_directory()) continue;
+                if (dir.path().filename() == ".git") continue;
+                if (dir.path().filename() == ".mypy_cache") continue;
+                if (dir.path().filename() == ".cache") continue;
+                if (dir.path().filename() == "__pycache__") continue;
+                if (dir.path().filename() == ".stack-work") continue;
 
-            work.push_back(dir.path());
+                work.push_back(dir.path());
+            }
+        }
+        catch (const filesystem_error& e) {
         }
     }
 
