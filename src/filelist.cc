a78da74e979a4499be0283b7df6e39fb39d443f0
Andreas Edlund <andreas@myrstacken.net>
2021-07-02 01:09:15 +0200

    Handle file not found error

--- src/filelist.cc
+++ src/filelist.cc
@@ -6,11 +6,19 @@
 
 using std::filesystem::recursive_directory_iterator;
 using std::filesystem::directory_options;
+using std::filesystem::filesystem_error;
 
 int main(int argc, char *argv[])
 {
-    for (auto& ent : recursive_directory_iterator("/mnt/extra", directory_options::skip_permission_denied)) {
-        std::cout << ent.path().string() << std::endl;
+    try {
+        for (auto& ent : recursive_directory_iterator("/mnt/extra", directory_options::skip_permission_denied)) {
+            std::cout << ent.path().string() << std::endl;
+        }
+    }
+    catch (const filesystem_error& e) {
+        if (e.code() != std::errc::no_such_file_or_directory) {
+            throw;
+        }
     }
 
     for (auto& ent : recursive_directory_iterator("/etc", directory_options::skip_permission_denied)) {
