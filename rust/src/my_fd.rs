029f5ded525f72f7fe885fc007219e5967ef9a7c
Andreas Edlund <andreas@myrstacken.net>
2023-01-17 07:48:48 +0100

    Strip working directory

--- rust/src/my_fd.rs
+++ rust/src/my_fd.rs
@@ -2,14 +2,9 @@ use std::path::Path;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
 
-/// Strip "./" from the beginning of a path. Do nothing if the path does not
-/// start with "./".
+/// Strip current working directory from the beginning of a path.
 fn strip_prefix(p: &Path) -> &Path {
-    if p.starts_with("./") {
-        p.strip_prefix("./").unwrap()
-    } else {
-        p
-    }
+    p.strip_prefix(std::env::current_dir().unwrap()).unwrap()
 }
 
 fn is_hidden(entry: &DirEntry) -> bool {
@@ -25,10 +20,11 @@ fn is_hidden(entry: &DirEntry) -> bool {
 }
 
 fn main() {
-    let walker = WalkDir::new(".").into_iter();
+    let cwd = std::env::current_dir().unwrap();
+
+    let walker = WalkDir::new(cwd).into_iter();
     for entry in walker.filter_entry(|e| !is_hidden(e)) {
         if let Ok(entry) = entry {
-            // Print the path of the file or directory without the leading "./"
             let path = entry.path();
             let path = strip_prefix(path);
 
