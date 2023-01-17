210f9bbf609ef7d97b1832e0c942f90d1af44415
Andreas Edlund <andreas@myrstacken.net>
2023-01-17 07:47:01 +0100

    Ignore hidded files

--- rust/src/my_fd.rs
+++ rust/src/my_fd.rs
@@ -1,5 +1,6 @@
-use walkdir::WalkDir;
 use std::path::Path;
+use walkdir::DirEntry;
+use walkdir::WalkDir;
 
 /// Strip "./" from the beginning of a path. Do nothing if the path does not
 /// start with "./".
@@ -11,17 +12,25 @@ fn strip_prefix(p: &Path) -> &Path {
     }
 }
 
+fn is_hidden(entry: &DirEntry) -> bool {
+    if entry.file_name() == "." || entry.file_name() == ".." {
+        return false;
+    }
+
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
 fn main() {
-    for entry in WalkDir::new(".") {
+    let walker = WalkDir::new(".").into_iter();
+    for entry in walker.filter_entry(|e| !is_hidden(e)) {
         if let Ok(entry) = entry {
             // Print the path of the file or directory without the leading "./"
             let path = entry.path();
             let path = strip_prefix(path);
-
-            // Ignore hidden files
-            if path.starts_with(".") {
-                continue;
-            }
 
             println!("{}", path.display());
         }
