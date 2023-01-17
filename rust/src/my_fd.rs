cef3dbb43310c1337ba3b20534e5c30af5d78081
Andreas Edlund <andreas@myrstacken.net>
2023-01-17 07:24:18 +0100

    Strip "./" prefix

--- rust/src/my_fd.rs
+++ rust/src/my_fd.rs
@@ -1,7 +1,19 @@
 use walkdir::WalkDir;
+use std::path::Path;
+
+/// Strip "./" from the beginning of a path. Do nothing if the path does not
+/// start with "./".
+fn strip_prefix(p: &Path) -> &Path {
+    if p.starts_with("./") {
+        p.strip_prefix("./").unwrap()
+    } else {
+        p
+    }
+}
 
 fn main() {
     for entry in WalkDir::new(".") {
-        println!("{}", entry.unwrap().path().display());
+        // Print the path of the file or directory without the leading "./"
+        println!("{}", strip_prefix(entry.unwrap().path()).display());
     }
 }
