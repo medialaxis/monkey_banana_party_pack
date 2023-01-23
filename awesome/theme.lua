160235b79c52bf278d990ac47d6c592bbaff3cb9
Andreas Edlund <andreas@myrstacken.net>
2023-01-23 22:23:36 +0100

    Disable application icons

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -31,6 +31,9 @@ theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
 theme.border_marked = "#91231c"
 
+-- Disable application icons
+theme.tasklist_disable_icon = true
+
 -- There are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
