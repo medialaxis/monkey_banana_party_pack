966c4985a7a3127d29fb5c8d00cbe55dcab7f530
Andreas Edlund <andreas@myrstacken.net>
2023-01-24 22:49:20 +0100

    Update plugins

--- awesome/plugins.sh
+++ awesome/plugins.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-clone()
+plugin()
 {
-    git clone https://github.com/lcpz/lain ~/.local/share/awesome/plugins/lain
-    pushd ~/.local/share/awesome/plugins/lain
-    git checkout 88f5a8abd2649b348ffec433a24a263b37f122c0 2> /dev/null
-    popd
+    local remote=$1
+    local commit=$2
 
+    local plugin_dir=~/.local/share/awesome/plugins/$(basename $remote)
+
+    if [ ! -d $plugin_dir ]; then
+        git clone $remote $plugin_dir
+    fi
+
+    pushd $plugin_dir > /dev/null
+    git fetch
+    git checkout $commit 2> /dev/null
+    popd > /dev/null
 }
+
+plugin https://github.com/lcpz/lain 88f5a8abd2649b348ffec433a24a263b37f122c0
 
 "$@"
