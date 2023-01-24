d89b67a259345bca989667c8ddbb4464c65823bb
Andreas Edlund <andreas@myrstacken.net>
2023-01-24 19:51:37 +0100

    Enable quit

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -290,8 +290,8 @@ globalkeys = gears.table.join(
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
---    awful.key({ modkey, "Shift"   }, "q", awesome.quit,
---              {description = "quit awesome", group = "awesome"}),
+    awful.key({ modkey, "Shift"   }, "q", awesome.quit,
+              {description = "quit awesome", group = "awesome"}),
 
     awful.key({ modkey, "Shift"   }, "j",     function () awful.tag.incmwfact(-0.05)          end,
               {description = "decrease master width factor", group = "layout"}),
