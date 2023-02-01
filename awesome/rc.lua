4ce8800fd26f67c262fed022fb7b39e7ecc9e8e0
Andreas Edlund <andreas@myrstacken.net>
2023-02-01 22:16:07 +0100

    Disabled the client list menu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -166,9 +166,9 @@ local tasklist_buttons = gears.table.join(
                                                   )
                                               end
                                           end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
+--                     awful.button({ }, 3, function()
+--                                              awful.menu.client_list({ theme = { width = 250 } })
+--                                          end),
                      awful.button({ }, 4, function ()
                                               awful.client.focus.byidx(1)
                                           end),
