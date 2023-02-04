7727427c60b34be699cff9a9462b1366b7a56217
Andreas Edlund <andreas@myrstacken.net>
2023-02-04 23:54:08 +0100

    Remove commented hotkeys thing

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -32,10 +32,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- Lain
 local lain = require("lain")
 
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
--- require("awful.hotkeys_popup.keys")
-
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
