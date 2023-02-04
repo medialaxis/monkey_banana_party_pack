3bde5ea742a0439c30b92c85ecb2338a3d9e1e76
Andreas Edlund <andreas@myrstacken.net>
2023-02-04 23:59:45 +0100

    Removed commented stuff

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -363,13 +363,8 @@ awful.rules.rules = {
 }
 -- }}}
 
--- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
-
     if awesome.startup
       and not c.size_hints.user_position
       and not c.size_hints.program_position then
@@ -420,4 +415,3 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
--- }}}
