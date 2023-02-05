84a8dd33ec98d3f736995b69754ae64acda5f121
Andreas Edlund <andreas@myrstacken.net>
2023-02-05 20:55:21 +0100

    Set solid background color

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -119,14 +119,15 @@ local tasklist_buttons = gears.table.join(
                                           end))
 
 local function set_wallpaper(s)
-    -- Wallpaper
     if beautiful.wallpaper then
         local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
         if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
+            gears.wallpaper.maximized(wallpaper(s), s, true)
+        elseif type(wallpaper) == "string" then
+            gears.wallpaper.set(wallpaper)
+        else
+            gears.wallpaper.maximized(wallpaper, s, true)
         end
-        gears.wallpaper.maximized(wallpaper, s, true)
     end
 end
 
--- awesome/theme.lua
+++ awesome/theme.lua
@@ -106,6 +106,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 theme.wallpaper = string.format("%s/wallpapers/simon_stålenhag_esso.jpg", home)
+-- theme.wallpaper = "#2596be"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
