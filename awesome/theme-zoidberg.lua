d08a58556680cf4e33f055bf9d023ce909167772
Andreas Edlund <andreas@myrstacken.net>
2024-03-22 07:09:22 +0100

    Use 20 as height for wibar

--- awesome/theme-zoidberg.lua
+++ awesome/theme-zoidberg.lua
@@ -73,7 +73,7 @@ theme.menu_submenu_icon = themes_path.."default/submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
 
-theme.wibar_height = dpi(30)
+theme.wibar_height = dpi(20)
 
 -- You can add as many variables as
 -- you wish and access them by using
